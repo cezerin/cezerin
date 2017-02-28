47dbff7146962332253f5bddb511c29ea71658ee
restmount <jsonmount@gmail.com>
2017-02-28 11:20:08 +0200

    Added shared Confirmation Dialog

---
+++ src/admin/client/modules/shared/confirmation/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class ConfirmationDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: props.open
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.state.open !== nextProps.open) {
+      this.setState({
+        open: nextProps.open
+      })
+    }
+  }
+
+  handleCancel = () => {
+    this.setState({open: false});
+    if(this.props.onCancel) {
+      this.props.onCancel();
+    }
+  }
+
+  handleSubmit = () => {
+    this.setState({open: false});
+    if(this.props.onSubmit) {
+      this.props.onSubmit();
+    }
+  }
+
+  render() {
+    const { title, description, submitLabel, cancelLabel, modal = false } = this.props;
+
+    const actions = [
+      <FlatButton
+        label={cancelLabel}
+        primary={true}
+        onTouchTap={this.handleCancel}
+      />,
+      <FlatButton
+        label={submitLabel}
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleSubmit}
+      />
+    ];
+
+    return (
+        <Dialog
+          title={title}
+          actions={actions}
+          modal={modal}
+          open={this.state.open}
+          onRequestClose={this.handleCancel}
+        >
+          <div style={{ wordWrap: 'break-word'}}>{description}</div>
+        </Dialog>
+    )
+  }
+}
