07c4be41e8f15b935d2be0ddec5dc215c299b5a3
restmount <jsonmount@gmail.com>
2017-03-27 18:09:50 +0300

    Move props to single file

--- src/store/shared/containers/checkout.js
+++ src/store/shared/containers/checkout.js
@@ -1,33 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {addCartItem, deleteCartItem, updateCartItemQuantiry, fetchMoreProducts} from '../actions'
+import {mapStateToProps, mapDispatchToProps} from '../containerProps'
 import {CheckoutContainer, config} from 'theme'
 import CheckoutForm from '../components/checkoutForm'
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    state: state.app
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    addCartItem: (item) => {
-      dispatch(addCartItem(item));
-    },
-    deleteCartItem: (item_id) => {
-      dispatch(deleteCartItem(item_id));
-    },
-    updateCartItemQuantiry: (item_id, quantity) => {
-      dispatch(updateCartItemQuantiry(item_id, quantity));
-    },
-    loadMoreProducts: () => {
-      dispatch(fetchMoreProducts());
-    }
-  }
-}
-
 const ConnectedCheckoutContainer = connect(mapStateToProps, mapDispatchToProps)(CheckoutContainer);
+
 export default() => {
   return <ConnectedCheckoutContainer checkoutForm={<CheckoutForm
     inputClassName={config.checkout_input_class}
--- src/store/shared/containers/product.js
+++ src/store/shared/containers/product.js
@@ -1,29 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {addCartItem, deleteCartItem, updateCartItemQuantiry, fetchMoreProducts} from '../actions'
+import {mapStateToProps, mapDispatchToProps} from '../containerProps'
 import {ProductContainer} from 'theme'
-
-const mapStateToProps = (state, ownProps) => {
-  return {
-    state: state.app
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    addCartItem: (item) => {
-      dispatch(addCartItem(item));
-    },
-    deleteCartItem: (item_id) => {
-      dispatch(deleteCartItem(item_id));
-    },
-    updateCartItemQuantiry: (item_id, quantity) => {
-      dispatch(updateCartItemQuantiry(item_id, quantity));
-    },
-    loadMoreProducts: () => {
-      dispatch(fetchMoreProducts());
-    }
-  }
-}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductContainer);
