08544588e7751aec99b736b19cd5419f6f66c43d
restmount <jsonmount@gmail.com>
2017-04-05 12:20:51 +0300

    Added more props

--- src/admin/client/modules/shared/form/index.js
+++ src/admin/client/modules/shared/form/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Toggle from 'material-ui/Toggle';
 
-export const CustomToggle = ({ input, label, className }) => (
+export const CustomToggle = ({ input, label, className = '', disabled = false, style }) => (
   <Toggle
     label={label}
     toggled={input.value ? true : false}
     onToggle={input.onChange}
     className={className}
+    disabled={disabled}
+    style={style}
   />
 )
