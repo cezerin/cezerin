fb88f323af583a99483cefad4d05f2dbd5d3777d
restmount <jsonmount@gmail.com>
2017-04-05 13:24:02 +0300

    Fixed #213

--- src/admin/client/modules/shared/editor/index.js
+++ src/admin/client/modules/shared/editor/index.js
@@ -12,19 +12,48 @@ const config = {
     toolbar2: 'undo redo | forecolor paste removeformat table | outdent indent | preview code'
   }
 
-const Editor = ({ input: { onChange, value } }) =>
-  <TinyMCE
-    content={value}
-    config={{
-      language: settings.language,
-      themes: config.themes,
-      inline: config.inline,
-      plugins: config.plugins,
-      toolbar1: config.toolbar1,
-      toolbar2: config.toolbar2,
-      menubar: false
-    }}
-    onChange={e => {onChange(e.target.getContent())}}
-  />
+export default class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.input.value
+    }
+    this.onChange = this.onChange.bind(this);
+  }
 
-export default Editor
+  componentWillReceiveProps(nextProps) {
+    if (this.state.value !== nextProps.input.value) {
+      this.setState({
+        value: nextProps.input.value
+      });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.value !== nextProps.input.value;
+  }
+
+  onChange = (e) => {
+    const content = e.target.getContent();
+    this.setState({ value: content });
+    this.props.input.onChange(content);
+  }
+
+  render() {
+    return (
+      <TinyMCE
+        content={this.state.value}
+        config={{
+          language: settings.language,
+          themes: config.themes,
+          inline: config.inline,
+          plugins: config.plugins,
+          toolbar1: config.toolbar1,
+          toolbar2: config.toolbar2,
+          menubar: false
+        }}
+        onChange={this.onChange}
+      />
+    )
+  }
+}
