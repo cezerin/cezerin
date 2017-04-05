cd7afc1107df11bc68418fedff1f62ac152fe079
restmount <jsonmount@gmail.com>
2017-04-05 11:47:08 +0300

    Added className

--- src/admin/client/modules/shared/form/index.js
+++ src/admin/client/modules/shared/form/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Toggle from 'material-ui/Toggle';
 
-export const CustomToggle = ({ input, label }) => (
+export const CustomToggle = ({ input, label, className }) => (
   <Toggle
     label={label}
     toggled={input.value ? true : false}
     onToggle={input.onChange}
+    className={className}
   />
 )
