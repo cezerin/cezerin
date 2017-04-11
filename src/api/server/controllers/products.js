d5d1875599a83755aa8d3ca19a756a2313eb6b62
restmount <jsonmount@gmail.com>
2017-04-11 19:34:57 +0300

    Added product options to API #218

--- src/api/server/controllers/products.js
+++ src/api/server/controllers/products.js
@@ -2,6 +2,8 @@
 
 const security = require('../lib/security');
 var ProductsService = require('../services/products/products');
+var ProductOptionsService = require('../services/products/options');
+var ProductOptionValuesService = require('../services/products/optionValues');
 
 class ProductsController {
   constructor(router) {
@@ -12,16 +14,28 @@ class ProductsController {
   registerRoutes() {
     this.router.get('/v1/products', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getProducts.bind(this));
     this.router.post('/v1/products', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.addProduct.bind(this));
-    this.router.get('/v1/products/:id', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getSingleProduct.bind(this));
-    this.router.put('/v1/products/:id', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.updateProduct.bind(this));
-    this.router.delete('/v1/products/:id', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.deleteProduct.bind(this));
+    this.router.get('/v1/products/:productId', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getSingleProduct.bind(this));
+    this.router.put('/v1/products/:productId', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.updateProduct.bind(this));
+    this.router.delete('/v1/products/:productId', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.deleteProduct.bind(this));
 
-    this.router.get('/v1/products/:id/images', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getProductImages.bind(this));
-    this.router.post('/v1/products/:id/images', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.addProductImage.bind(this));
-    this.router.delete('/v1/products/:id/images/:image', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.deleteProductImage.bind(this));
+    this.router.get('/v1/products/:productId/images', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getProductImages.bind(this));
+    this.router.post('/v1/products/:productId/images', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.addProductImage.bind(this));
+    this.router.delete('/v1/products/:productId/images/:image', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.deleteProductImage.bind(this));
 
-    this.router.get('/v1/products/:id/sku', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.isSkuExists.bind(this));
-    this.router.get('/v1/products/:id/slug', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.isSlugExists.bind(this));
+    this.router.get('/v1/products/:productId/sku', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.isSkuExists.bind(this));
+    this.router.get('/v1/products/:productId/slug', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.isSlugExists.bind(this));
+
+    this.router.get('/v1/products/:productId/options', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getOptions.bind(this));
+    this.router.get('/v1/products/:productId/options/:optionId', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getSingleOption.bind(this));
+    this.router.post('/v1/products/:productId/options', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.addOption.bind(this));
+    this.router.put('/v1/products/:productId/options/:optionId', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.updateOption.bind(this));
+    this.router.delete('/v1/products/:productId/options/:optionId', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.deleteOption.bind(this));
+
+    this.router.get('/v1/products/:productId/options/:optionId/values', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getOptionValues.bind(this));
+    this.router.get('/v1/products/:productId/options/:optionId/values/:valueId', security.checkUserScope.bind(this, security.scope.READ_PRODUCTS), this.getSingleOptionValue.bind(this));
+    this.router.post('/v1/products/:productId/options/:optionId/values', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.addOptionValue.bind(this));
+    this.router.put('/v1/products/:productId/options/:optionId/values/:valueId', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.updateOptionValue.bind(this));
+    this.router.delete('/v1/products/:productId/options/:optionId/values/:valueId', security.checkUserScope.bind(this, security.scope.WRITE_PRODUCTS), this.deleteOptionValue.bind(this));
   }
 
   getProducts(req, res, next) {
@@ -31,7 +45,7 @@ class ProductsController {
   }
 
   getSingleProduct(req, res, next) {
-    ProductsService.getSingleProduct(req.params.id).then(data => {
+    ProductsService.getSingleProduct(req.params.productId).then(data => {
       if (data) {
         res.send(data)
       } else {
@@ -47,7 +61,7 @@ class ProductsController {
   }
 
   updateProduct(req, res, next) {
-    ProductsService.updateProduct(req.params.id, req.body).then(data => {
+    ProductsService.updateProduct(req.params.productId, req.body).then(data => {
       if (data) {
         res.send(data)
       } else {
@@ -57,7 +71,7 @@ class ProductsController {
   }
 
   deleteProduct(req, res, next) {
-    ProductsService.deleteProduct(req.params.id).then(data => {
+    ProductsService.deleteProduct(req.params.productId).then(data => {
       res.status(data
         ? 200
         : 404).end()
@@ -65,7 +79,7 @@ class ProductsController {
   }
 
   getProductImages(req, res, next) {
-    ProductsService.getProductImages(req.params.id).then(data => {
+    ProductsService.getProductImages(req.params.productId).then(data => {
       res.send(data)
     }).catch(next);
   }
@@ -75,13 +89,13 @@ class ProductsController {
   }
 
   deleteProductImage(req, res, next) {
-    ProductsService.deleteProductImage(req.params.id, req.params.image).then(data => {
+    ProductsService.deleteProductImage(req.params.productId, req.params.image).then(data => {
       res.end()
     });
   }
 
   isSkuExists(req, res, next) {
-    ProductsService.isSkuExists(req.query.sku, req.params.id).then(exists => {
+    ProductsService.isSkuExists(req.query.sku, req.params.productId).then(exists => {
       res.status(exists
         ? 200
         : 404).end()
@@ -89,12 +103,81 @@ class ProductsController {
   }
 
   isSlugExists(req, res, next) {
-    ProductsService.isSlugExists(req.query.slug, req.params.id).then(exists => {
+    ProductsService.isSlugExists(req.query.slug, req.params.productId).then(exists => {
       res.status(exists
         ? 200
         : 404).end()
     }).catch(next);
   }
+
+  getOptions(req, res, next) {
+    ProductOptionsService.getOptions(req.params.productId).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  getSingleOption(req, res, next) {
+    ProductOptionsService.getSingleOption(req.params.productId, req.params.optionId).then(data => {
+      if (data) {
+        res.send(data)
+      } else {
+        res.status(404).end()
+      }
+    }).catch(next);
+  }
+
+  addOption(req, res, next) {
+    ProductOptionsService.addOption(req.params.productId, req.body).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  updateOption(req, res, next) {
+    ProductOptionsService.updateOption(req.params.productId, req.params.optionId, req.body).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  deleteOption(req, res, next) {
+    ProductOptionsService.deleteOption(req.params.productId, req.params.optionId).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  getOptionValues(req, res, next) {
+    ProductOptionValuesService.getOptionValues(req.params.productId, req.params.optionId).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  getSingleOptionValue(req, res, next) {
+    ProductOptionValuesService.getSingleOptionValue(req.params.productId, req.params.optionId, req.params.valueId).then(data => {
+      if (data) {
+        res.send(data)
+      } else {
+        res.status(404).end()
+      }
+    }).catch(next);
+  }
+
+  addOptionValue(req, res, next) {
+    ProductOptionValuesService.addOptionValue(req.params.productId, req.params.optionId, req.body).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  updateOptionValue(req, res, next) {
+    ProductOptionValuesService.updateOptionValue(req.params.productId, req.params.optionId, req.params.valueId, req.body).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  deleteOptionValue(req, res, next) {
+    ProductOptionValuesService.deleteOptionValue(req.params.productId, req.params.optionId, req.params.valueId).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
 }
 
 module.exports = ProductsController;
---
+++ src/api/server/services/products/optionValues.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var mongo = require('../../lib/mongo');
+var parse = require('../../lib/parse');
+var ObjectID = require('mongodb').ObjectID;
+
+class ProductOptionValuesService {
+  constructor() {}
+
+  getOptionValues(productId, optionId) {
+    let productObjectID = new ObjectID(productId);
+
+    return mongo.db.collection('products').findOne({ _id: productObjectID }, {fields: {options: 1}})
+      .then(product => product && product.options ? product.options : null)
+      .then(options => options && options.length > 0 ? options.find(option => option.id.toString() === optionId) : null)
+      .then(option => option && option.values.length > 0 ? option.values : [])
+  }
+
+  getSingleOptionValue(productId, optionId, valueId) {
+    return this.getOptionValues(productId, optionId).then(optionValues => optionValues.find(optionValue => optionValue.id.toString() === valueId))
+  }
+
+  addOptionValue(productId, optionId, data) {
+    if(!ObjectID.isValid(productId) || !ObjectID.isValid(optionId)) {
+      return Promise.reject('Invalid identifier');
+    }
+    let productObjectID = new ObjectID(productId);
+    let optionObjectID = new ObjectID(optionId);
+
+    const optionValueData = this.getValidDocumentForInsert(data);
+
+    return mongo.db.collection('products').updateOne({
+        _id: productObjectID, 'options.id': optionObjectID
+      }, {$push: { 'options.$.values': optionValueData }}).then(res => this.getOptionValues(productId, optionId));
+  }
+
+  updateOptionValue(productId, optionId, valueId, data) {
+    if(!ObjectID.isValid(productId) || !ObjectID.isValid(optionId) || !ObjectID.isValid(valueId)) {
+      return Promise.reject('Invalid identifier');
+    }
+
+    if(data.name !== undefined) {
+      return this.getModifiedOptionValues(productId, optionId, valueId, data.name)
+        .then(values => this.overwriteAllValuesForOption(productId, optionId, values))
+        .then(updateResult => this.getOptionValues(productId, optionId))
+    } else {
+      return Promise.reject('Please, specify value name')
+    }
+
+  }
+
+  deleteOptionValue(productId, optionId, valueId) {
+    if(!ObjectID.isValid(productId) || !ObjectID.isValid(optionId) || !ObjectID.isValid(valueId)) {
+      return Promise.reject('Invalid identifier');
+    }
+
+    return this.getOptionValuesWithDeletedOne(productId, optionId, valueId)
+      .then(values => this.overwriteAllValuesForOption(productId, optionId, values))
+      .then(updateResult => this.getOptionValues(productId, optionId))
+  }
+
+  getModifiedOptionValues(productId, optionId, valueId, name){
+    return this.getOptionValues(productId, optionId).then(values => {
+      if(values && values.length > 0) {
+        values = values.map(value => {
+          if(value.id.toString() === valueId) {
+            value.name = name;
+            return value;
+          } else {
+            return value
+          }
+        });
+      }
+
+      return values;
+    })
+  }
+
+  getOptionValuesWithDeletedOne(productId, optionId, deleteValueId){
+    return this.getOptionValues(productId, optionId).then(values => {
+      if(values && values.length > 0) {
+        values = values.filter(value => value.id.toString() !== deleteValueId);
+      }
+
+      return values;
+    })
+  }
+
+  overwriteAllValuesForOption(productId, optionId, values){
+    let productObjectID = new ObjectID(productId);
+    let optionObjectID = new ObjectID(optionId);
+
+    if(!values) {
+      return;
+    }
+
+    return mongo.db.collection('products').updateOne(
+      { _id: productObjectID, 'options.id': optionObjectID},
+      { $set: { 'options.$.values': values }});
+  }
+
+  getValidDocumentForInsert(data) {
+    let optionValue = {
+      id: new ObjectID(),
+      name: parse.getString(data.name)
+    };
+
+    return optionValue;
+  }
+}
+
+module.exports = new ProductOptionValuesService();
