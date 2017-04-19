d352e9d314743802ca793b41bab5c14a5096fc4b
restmount <jsonmount@gmail.com>
2017-04-19 12:59:42 +0300

    Added categories full tree support

--- themes/current/src/components/categoryTree.js
+++ themes/current/src/components/categoryTree.js
@@ -5,14 +5,25 @@ import config from '../lib/config'
 import * as helper from '../lib/helper'
 
 const TreeItem = ({ categories, categoryDetails, activeCategory, onClick }) => {
-  const childs = categories.filter(category => category.parent_id === categoryDetails.id).map((category, index) => <TreeItem key={index} categories={categories} categoryDetails={category} activeCategory={activeCategory} />)
-  const currentCategoryIsActive = activeCategory && activeCategory.id === categoryDetails.id;
-  const parentCategoryIsActive = activeCategory && activeCategory.parent_id === categoryDetails.id;
+  const childs = categories
+    .filter(category => category.parent_id === categoryDetails.id)
+    .map((category, index) =>
+      <TreeItem key={index} categories={categories} categoryDetails={category} activeCategory={activeCategory} />
+    );
+
+  const parentIds = helper.getParentIds(categories, activeCategory.id);
+  const isActive = parentIds.includes(categoryDetails.id) || activeCategory.id === categoryDetails.id;
+  const hasChild = childs && childs.length > 0;
+
+  let classNames = isActive ? 'active' : '';
+  if(hasChild){
+    classNames += classNames === '' ? 'has-child' : ' has-child';
+  }
 
   return (
-    <li className={currentCategoryIsActive || parentCategoryIsActive ? 'active' : ''}>
+    <li className={classNames}>
       <Link to={categoryDetails.path} activeClassName="active" onClick={onClick}>{categoryDetails.name}</Link>
-      {childs && childs.length > 0 &&
+      {hasChild &&
         <ul>
           {childs}
         </ul>
@@ -23,7 +34,12 @@ const TreeItem = ({ categories, categoryDetails, activeCategory, onClick }) => {
 
 const CategoryTree = ({ categories, activeCategory, onClick }) => {
   if(categories) {
-    const elements = categories.filter(category => category.parent_id === null).map((category, index) => <TreeItem key={index} categories={categories} categoryDetails={category} activeCategory={activeCategory} onClick={onClick} />)
+    const elements = categories
+      .filter(category => category.parent_id === null)
+      .map((category, index) =>
+        <TreeItem key={index} categories={categories} categoryDetails={category} activeCategory={activeCategory} onClick={onClick} />
+      );
+
     return (
       <div className="categories-tree">
         <ul>
--- themes/current/src/lib/helper.js
+++ themes/current/src/lib/helper.js
@@ -26,3 +26,19 @@ export const getThumbnailUrl = (originalUrl, width) => {
     return '';
   }
 }
+
+export const getParentIds = (categories, categoryId) => {
+  let parentIds = [];
+  let parentExists = false;
+
+  do {
+    const category = categories.find(item => item.id === categoryId);
+    parentExists = category && category.parent_id;
+    if(parentExists){
+      parentIds.push(category.parent_id)
+      categoryId = category.parent_id;
+    }
+  } while(parentExists)
+
+  return parentIds;
+}
