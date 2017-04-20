fd18500105df05a9dcc3ba82881ed7cdb10c7b91
restmount <jsonmount@gmail.com>
2017-04-20 17:29:11 +0300

    Added must have folders

--- config/admin.js
+++ config/admin.js
@@ -1,11 +1,6 @@
 // config used by dashboard client side only
-let domain = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
 module.exports = {
   // dashboard UI language
   language: 'en',
-<<<<<<< HEAD
   apiBaseUrl: '/api/v1'
-=======
-  apiBaseUrl: `${domain}/api/v1`
->>>>>>> ca2e58733491181e93ba2f35bb57f7f67df76ebd
 }
---
+++ public/static/README.md
@@ -0,0 +1,1 @@
+Folder must exists
