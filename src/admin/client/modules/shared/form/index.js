80d48b9ab947e12983e5b0d901f86ac8f2d80714
restmount <jsonmount@gmail.com>
2017-05-16 21:45:16 +0300

    Fixed Toggle when value is false

--- src/admin/client/modules/shared/form/index.js
+++ src/admin/client/modules/shared/form/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Toggle from 'material-ui/Toggle';
 
-export const CustomToggle = ({ input, label, className = '', disabled = false, style }) => (
-  <Toggle
-    label={label}
-    toggled={input.value ? true : false}
-    onToggle={input.onChange}
-    className={className}
-    disabled={disabled}
-    style={style}
-  />
-)
+export const CustomToggle = ({ input, label, className = '', disabled = false, style }) => {
+  return (
+    <Toggle
+      label={label}
+      toggled={input.value ? true : false}
+      onToggle={(event, isInputChecked) => {
+        input.onChange(isInputChecked)
+      }}
+      className={className}
+      disabled={disabled}
+      style={style}
+    />
+  )
+}
