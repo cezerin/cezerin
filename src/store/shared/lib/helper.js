65d214aeaeb58e5b79704c79afc3846024f83d4f
restmount <jsonmount@gmail.com>
2017-05-17 22:55:15 +0300

    Added currency and numbers format methods

--- src/store/shared/lib/helper.js
+++ src/store/shared/lib/helper.js
@@ -1,3 +1,32 @@
+export const formatNumber = (number = 0, settings) => {
+  const x = 3;
+
+  const re = '\\d(?=(\\d{' + x + '})+' + (settings.decimal_number > 0
+    ? '\\D'
+    : '$') + ')';
+
+  let num = (number || 0).toFixed(Math.max(0, ~~ settings.decimal_number));
+
+  return (settings.decimal_separator
+    ? num.replace('.', settings.decimal_separator)
+    : num).replace(new RegExp(re, 'g'), '$&' + (settings.thousand_separator));
+};
+
+const amountPattern = '{amount}';
+export const formatCurrency = (number = 0, settings) => {
+  return settings.currency_format.replace(amountPattern, formatNumber(number, settings));
+}
+
+export const getThumbnailUrl = (originalUrl, width) => {
+  if(originalUrl && originalUrl.length > 0) {
+    const pos = originalUrl.lastIndexOf('/');
+    const thumbnailUrl = originalUrl.substring(0, pos) + `/${width}/` + originalUrl.substring(pos + 1);
+    return thumbnailUrl;
+  } else {
+    return '';
+  }
+}
+
 export const getParentIds = (categories, categoryId) => {
   let parentIds = [];
   let parentExists = false;
