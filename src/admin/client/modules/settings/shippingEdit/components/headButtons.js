ee0021ccd963f824e7895d5569b1d8c13a97b46a
restmount <jsonmount@gmail.com>
2017-06-08 11:58:50 +0300

    Change tooltip position

--- src/admin/client/modules/settings/shippingEdit/components/headButtons.js
+++ src/admin/client/modules/settings/shippingEdit/components/headButtons.js
@@ -49,7 +49,7 @@ export default class Buttons extends React.Component {
 
     return (
       <span>
-        <IconButton touch={true} tooltip={messages.actions_delete} onTouchTap={this.showDelete}>
+        <IconButton touch={true} tooltipPosition="bottom-left" tooltip={messages.actions_delete} onTouchTap={this.showDelete}>
           <FontIcon color="#fff" className="material-icons">delete</FontIcon>
         </IconButton>
 
