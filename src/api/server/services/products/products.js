fae61dc033213afc7c49f19c62f661fd5d0928af
restmount <jsonmount@gmail.com>
2017-06-20 16:56:07 +0300

    Fixed ids

--- src/api/server/services/products/products.js
+++ src/api/server/services/products/products.js
@@ -385,6 +385,7 @@ class ProductsService {
      on_sale = parse.getBooleanIfValid(on_sale);
      price_from = parse.getNumberIfPositive(price_from);
      price_to = parse.getNumberIfPositive(price_to);
+     ids = ids.toString();
 
      let queries = [];
      const currentDate = new Date();
