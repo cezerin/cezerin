3d74ea07d7722cb6c991692fd329b56a02da3b60
restmount <jsonmount@gmail.com>
2017-09-08 13:48:16 +0300

    Get Payment Form Settings through network only

--- public/sw.js
+++ public/sw.js
@@ -4,4 +4,5 @@ toolbox.router.get('/static/*', toolbox.cacheFirst);
 toolbox.router.get('/assets/*', toolbox.cacheFirst);
 toolbox.router.get('/admin-assets/*', toolbox.cacheFirst);
 toolbox.router.get('/api/*', toolbox.networkOnly);
+toolbox.router.get('/ajax/payment_form_settings', toolbox.networkOnly);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
