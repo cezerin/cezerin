db6f293e55e2b4ac9b6095894eb4619894b96626
restmount <jsonmount@gmail.com>
2017-10-09 07:40:39 +0300

    Added upload and delete theme assets from API

--- src/api/server/controllers/theme.js
+++ src/api/server/controllers/theme.js
@@ -3,6 +3,7 @@
 const security = require('../lib/security');
 var ThemeService = require('../services/theme/theme');
 var ThemeSettingsService = require('../services/theme/settings');
+var ThemeAssetsService = require('../services/theme/assets');
 
 class ThemeController {
   constructor(router) {
@@ -17,6 +18,9 @@ class ThemeController {
     this.router.get('/v1/theme/settings', security.checkUserScope.bind(this, security.scope.READ_THEME), this.getSettings.bind(this));
     this.router.put('/v1/theme/settings', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.updateSettings.bind(this));
     this.router.get('/v1/theme/settings_schema', security.checkUserScope.bind(this, security.scope.READ_THEME), this.getSettingsSchema.bind(this));
+
+    this.router.post('/v1/theme/assets', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.uploadFile.bind(this));
+    this.router.delete('/v1/theme/assets/:file', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.deleteFile.bind(this));
   }
 
   exportTheme(req, res, next) {
@@ -42,6 +46,16 @@ class ThemeController {
   getSettingsSchema(req, res, next) {
     ThemeSettingsService.getSettingsSchema().then(data => {
       res.send(data)
+    }).catch(next)
+  }
+
+  uploadFile(req, res, next) {
+    ThemeAssetsService.uploadFile(req, res, next);
+  }
+
+  deleteFile(req, res, next) {
+    ThemeAssetsService.deleteFile(req.params.file).then(() => {
+      res.end()
     }).catch(next)
   }
 }
---
+++ src/api/server/services/theme/assets.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const path = require('path');
+const fsExtra = require('fs-extra');
+const fs = require('fs');
+const url = require('url');
+var formidable = require('formidable');
+const settings = require('../../lib/settings');
+
+class ThemeAssetsService {
+  deleteFile(fileName) {
+    return new Promise((resolve, reject) => {
+      const filePath = path.resolve(settings.themeAssetsUploadPath + '/' + fileName);
+      if(fs.existsSync(filePath)){
+        fs.unlink(filePath, (err) => {
+          resolve();
+        })
+      } else {
+        reject('File not found');
+      }
+    });
+  }
+
+  uploadFile(req, res, next) {
+    const uploadDir = path.resolve(settings.themeAssetsUploadPath);
+
+    let form = new formidable.IncomingForm(),
+        file_name = null,
+        file_size = 0;
+
+    form.uploadDir = uploadDir;
+
+    form
+      .on('fileBegin', (name, file) => {
+        // Emitted whenever a field / value pair has been received.
+        file.path = uploadDir + '/' + file.name;
+      })
+      .on('file', function(field, file) {
+        // every time a file has been uploaded successfully,
+        file_name = file.name;
+        file_size = file.size;
+      })
+      .on('error', (err) => {
+        res.status(500).send(this.getErrorMessage(err));
+      })
+      .on('end', () => {
+        //Emitted when the entire request has been received, and all contained files have finished flushing to disk.
+        if(file_name) {
+          res.send({ 'file': file_name, 'size': file_size });
+        } else {
+          res.status(400).send(this.getErrorMessage('Required fields are missing'));
+        }
+      });
+
+    form.parse(req);
+  }
+
+  getErrorMessage(err) {
+    return { 'error': true, 'message': err.toString() };
+  }
+}
+
+module.exports = new ThemeAssetsService();
