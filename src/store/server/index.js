cccef1c4f64c4ffe6dfdfea5a6bd4c5ffc1c86f9
restmount <jsonmount@gmail.com>
2017-10-21 10:30:21 +0300

    Added redirects to store

--- src/store/server/index.js
+++ src/store/server/index.js
@@ -4,9 +4,11 @@ let storeRouter = express.Router();
 import robotsRendering from './robotsRendering'
 import sitemapRendering from './sitemapRendering'
 import pageRendering from './pageRendering'
+import redirects from './redirects'
 
 storeRouter.get('/robots.txt', robotsRendering)
 storeRouter.get('/sitemap.xml', sitemapRendering);
+storeRouter.get('*', redirects);
 storeRouter.get('*', pageRendering);
 
 module.exports = storeRouter;
---
+++ src/store/server/redirects.js
@@ -0,0 +1,47 @@
+import api from './api'
+
+const IGNORE_PATH = ['/'];
+
+const getRedirect = (req) => {
+  const absoluteUrl = `${req.protocol}://${req.hostname}${req.url}`;
+  const relativeUrl = req.url;
+  const relativePath = req.path;
+
+  return api.redirects.list().then(({ status, json }) => {
+    const items = json;
+    if(items && items.length > 0){
+      /*
+      1. check absolute url
+      2. check relative url
+      3. check relative url (without query)
+      */
+      const redirect = items.find(item => item.from === absoluteUrl || item.from === relativeUrl || item.from === relativePath);
+      return redirect;
+    }
+
+    return null;
+  });
+}
+
+const redirectUrlIsValid = (url) => {
+  return url && url.length > 0 && (url.startsWith('/') || url.startsWith('https://') || url.startsWith('http://'));
+}
+
+const redirects = (req, res, next) => {
+  if(IGNORE_PATH.includes(req.url)){
+    next();
+  } else {
+    getRedirect(req).then(redirect => {
+      if(redirect && redirectUrlIsValid(redirect.to)){
+        res.redirect(redirect.status, redirect.to);
+      } else {
+        next();
+      }
+    })
+    .catch(() => {
+      next();
+    })
+  }
+}
+
+export default redirects;
