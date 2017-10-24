338aa6eb8dc0634bac871731814b6a7466e38dc1
restmount <jsonmount@gmail.com>
2017-10-24 22:20:11 +0300

    Added shipping fields to checkout form

--- src/store/shared/components/stepShipping/form.js
+++ src/store/shared/components/stepShipping/form.js
@@ -16,13 +16,36 @@ const inputField = (field) => (
 
 const textareaField = (field) => (
   <div className={field.className}>
-    <label htmlFor={field.id}>{field.label}</label>
+    <label htmlFor={field.id}>{field.label}{field.meta.touched && field.meta.error && <span className="error">{field.meta.error}</span>}</label>
     <textarea {...field.input} placeholder={field.placeholder} rows={field.rows} id={field.id} className={field.meta.touched && field.meta.error
       ? "invalid"
       : ""}></textarea>
-    {field.meta.touched && field.meta.error && <div className="error">{field.meta.error}</div>}
   </div>
 )
+
+const getFieldLabelByKey = (key) => {
+  switch (key) {
+    case 'full_name':
+      return text.fullName;
+    case 'address1':
+      return text.address1;
+    case 'address2':
+      return text.address2;
+    case 'postal_code':
+      return text.postal_code;
+    case 'phone':
+      return text.phone;
+    case 'company':
+      return text.company;
+    default:
+      return '';
+  }
+}
+
+const getFieldLabel = (field) => {
+  const label = field.label && field.label.length > 0 ? field.label : getFieldLabelByKey(field.key);
+  return field.required === true ? label : `${label} (${text.optional})`;
+}
 
 class CheckoutStepShipping extends React.Component {
   constructor(props) {
@@ -71,6 +94,8 @@ class CheckoutStepShipping extends React.Component {
       submitting,
       processingCheckout,
       initialValues,
+      shippingMethod,
+      checkoutFields,
       settings,
       finishCheckout,
       inputClassName,
@@ -82,6 +107,13 @@ class CheckoutStepShipping extends React.Component {
     const { payment_method_gateway, grand_total } = initialValues;
     const showPaymentForm = payment_method_gateway && payment_method_gateway !== '';
 
+    const commentsField = checkoutFields.find(f => f.name === 'comments');
+    const commentsFieldPlaceholder = commentsField && commentsField.placeholder && commentsField.placeholder.length > 0 ? commentsField.placeholder : '';
+    const commentsFieldLabel = commentsField && commentsField.label && commentsField.label.length > 0 ? commentsField.label : text.comments;
+    const commentsFieldStatus = commentsField && commentsField.status.length > 0 ? commentsField.status : null;
+    const commentsValidate = commentsFieldStatus === 'required' ? validateRequired : null;
+    const hideCommentsField = commentsFieldStatus === 'hidden';
+
     if(!this.props.show){
       return (
         <div className="checkout-step">
@@ -89,26 +121,31 @@ class CheckoutStepShipping extends React.Component {
         </div>
       )
     } else if(this.state.done){
+
+      let shippingFields = null;
+      if(shippingMethod && shippingMethod.fields && shippingMethod.fields.length > 0){
+        shippingFields = shippingMethod.fields.map((field, index) => {
+          const fieldLabel = getFieldLabel(field);
+          const fieldValue = initialValues.shipping_address[field.key];
+
+          return <div key={index} className="checkout-field-preview">
+            <div className="name">{fieldLabel}</div>
+            <div className="value">{fieldValue}</div>
+          </div>
+        })
+      }
+
       return (
         <div className="checkout-step">
           <h1><span>2</span>{this.props.title}</h1>
+          {shippingFields}
 
-          <div className="checkout-field-preview">
-            <div className="name">{text.shippingAddress}</div>
-            <div className="value">
-              <div>{initialValues.shipping_address.full_name}</div>
-              <div>{initialValues.shipping_address.company}</div>
-              <div>{initialValues.shipping_address.address1}</div>
-              <div>{initialValues.shipping_address.address2}</div>
-              <div>{initialValues.shipping_address.city}, {initialValues.shipping_address.state && initialValues.shipping_address.state.length > 0 ? initialValues.shipping_address.state + ', ' : ''}{initialValues.shipping_address.postal_code}</div>
-              <div>{initialValues.shipping_address.phone}</div>
+          {!hideCommentsField && initialValues.comments !== '' &&
+            <div className="checkout-field-preview">
+              <div className="name">{commentsFieldLabel}</div>
+              <div className="value">{initialValues.comments}</div>
             </div>
-          </div>
-
-          <div className="checkout-field-preview">
-            <div className="name">{text.comments}</div>
-            <div className="value">{initialValues.comments}</div>
-          </div>
+          }
 
           <div className="checkout-button-wrap">
             <button
@@ -121,17 +158,47 @@ class CheckoutStepShipping extends React.Component {
         </div>
       )
     } else {
+
+      let shippingFields = null;
+      if(shippingMethod && shippingMethod.fields && shippingMethod.fields.length > 0){
+        shippingFields = shippingMethod.fields.map((field, index) => {
+          const fieldLabel = getFieldLabel(field);
+          const fieldId = `shipping_address.${field.key}`;
+          const fieldClassName = `${inputClassName} shipping-${field.key}`;
+          const validate = field.required === true ? validateRequired : null;
+
+          return <Field
+            key={index}
+            className={fieldClassName}
+            name={fieldId}
+            id={fieldId}
+            component={inputField}
+            type="text"
+            label={fieldLabel}
+            validate={validate}
+          />
+        })
+      }
+
       return (
         <div className="checkout-step">
           <h1><span>2</span>{this.props.title}</h1>
           <form onSubmit={handleSubmit}>
-            <Field className={inputClassName + ' shipping-fullname'} name="shipping_address.full_name" id="shipping_address.full_name" component={inputField} type="text" label={text.fullName} validate={[validateRequired]}/>
-            <Field className={inputClassName + ' shipping-address1'} name="shipping_address.address1" id="shipping_address.address1" component={inputField} type="text" label={text.address1} validate={[validateRequired]}/>
-            <Field className={inputClassName + ' shipping-address2'} name="shipping_address.address2" id="shipping_address.address2" component={inputField} type="text" label={text.address2 + ` (${text.optional})`}/>
-            <Field className={inputClassName + ' shipping-postalcode'} name="shipping_address.postal_code" id="shipping_address.postal_code" component={inputField} type="text" label={text.postal_code + ` (${text.optional})`}/>
-            <Field className={inputClassName + ' shipping-phone'} name="shipping_address.phone" id="shipping_address.phone" component={inputField} type="text" label={text.phone + ` (${text.optional})`}/>
-            <Field className={inputClassName + ' shipping-company'} name="shipping_address.company" id="shipping_address.company" component={inputField} type="text" label={text.company + ` (${text.optional})`}/>
-            <Field className={inputClassName + ' shipping-comments'} name="comments" id="customer.comments" component={textareaField} type="text" label={text.comments} rows="3"/>
+            {shippingFields}
+
+            {!hideCommentsField &&
+              <Field
+                className={inputClassName + ' shipping-comments'}
+                name="comments"
+                id="customer.comments"
+                component={textareaField}
+                type="text"
+                label={commentsFieldLabel}
+                placeholder={commentsFieldPlaceholder}
+                validate={commentsValidate}
+                rows="3"
+              />
+            }
 
             {!hideBillingAddress &&
               <div>
--- src/store/shared/components/stepShipping/index.js
+++ src/store/shared/components/stepShipping/index.js
@@ -6,9 +6,17 @@ import {checkout, updateShipping} from '../../actions'
 import Form from './form'
 
 const mapStateToProps = (state, ownProps) => {
+  let shippingMethod = null;
+  const { shipping_method_id } = state.app.cart;
+  if(shipping_method_id && state.app.shippingMethods && state.app.shippingMethods.length > 0){
+    shippingMethod = state.app.shippingMethods.find(method => method.id === shipping_method_id);
+  }
+
   return {
+    shippingMethod: shippingMethod,
     initialValues: state.app.cart,
     settings: state.app.settings,
+    checkoutFields: state.app.checkoutFields,
     processingCheckout: state.app.processingCheckout
   }
 }
