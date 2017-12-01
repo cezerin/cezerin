f716d0a653202357c9549fcb7696bd5b89cff957
sonufrienko <sergey@onufrienko.com>
2017-12-01 18:09:46 +0200

    Added component to choose product quantity

--- theme/src/components/productDetails.js
+++ theme/src/components/productDetails.js
@@ -8,6 +8,7 @@ import ProductBreadcrumbs from './productBreadcrumbs'
 import DiscountCountdown from './discountCountdown'
 import CustomProductList from './customProductList'
 import Lightbox from 'react-image-lightbox'
+const Fragment = React.Fragment;
 
 const ProductOption = ({ option, onChange }) => {
   const values = option.values
@@ -175,6 +176,61 @@ const ProductTags = ({ tags }) => {
   }
 }
 
+class ProductQuantity extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.state.quantity > nextProps.maxQuantity){
+      this.setQuantity(nextProps.maxQuantity);
+    }
+  }
+
+  handleChange = (event) => {
+    this.setQuantity(event.target.value);
+  }
+
+  setQuantity = (quantity) => {
+    const intQuantity = parseInt(quantity);
+    if(intQuantity > 0 && intQuantity <= this.props.maxQuantity){
+      this.setState({ quantity: intQuantity });
+      this.props.onChange(intQuantity);
+    }
+  }
+
+  increment = () => {
+    const newQuantity = this.state.quantity + 1;
+    this.setQuantity(newQuantity);
+  }
+
+  decrement = () => {
+    const newQuantity = this.state.quantity - 1;
+    this.setQuantity(newQuantity);
+  }
+
+  render() {
+    const { maxQuantity } = this.props;
+    const { quantity } = this.state;
+    const disabled = maxQuantity === 0;
+    const value = disabled ? 0 : quantity;
+
+    return (
+      <Fragment>
+        <div>{text.qty}</div>
+        <div className="product-quantity">
+          <a className="decrement" onClick={this.decrement}></a>
+          <input value={value} onChange={this.handleChange} maxLength="3" type="number" pattern="\d*" disabled={disabled} />
+          <a className="increment" onClick={this.increment}></a>
+        </div>
+      </Fragment>
+    )
+  }
+}
+
 class ProductGallery extends React.Component {
   constructor(props) {
     super(props);
@@ -256,7 +312,8 @@ export default class ProductDetails extends React.Component {
     this.state = {
       selectedOptions: {},
       selectedVariant: null,
-      isAllOptionsSelected: false
+      isAllOptionsSelected: false,
+      quantity: 1
     }
 
     this.onOptionChange = this.onOptionChange.bind(this);
@@ -293,14 +350,17 @@ export default class ProductDetails extends React.Component {
     this.setState({ selectedVariant: null });
   }
 
+  setQuantity = (quantity) => {
+    this.setState({ quantity: quantity });
+  }
 
   addToCart() {
     const {product, addCartItem} = this.props;
-    const {selectedVariant} = this.state;
+    const {selectedVariant, quantity} = this.state;
 
     let item = {
       product_id: product.id,
-      quantity: 1
+      quantity: quantity
     }
 
     if(selectedVariant) {
@@ -321,6 +381,7 @@ export default class ProductDetails extends React.Component {
   render() {
     const {product, settings, categories} = this.props;
     const {selectedVariant, isAllOptionsSelected} = this.state;
+    const maxQuantity = selectedVariant ? selectedVariant.stock_quantity : product.stock_quantity;
 
     if(product){
       return (
@@ -348,6 +409,7 @@ export default class ProductDetails extends React.Component {
                       <ProductOptions options={product.options} onChange={this.onOptionChange} />
                     }
 
+                    <ProductQuantity maxQuantity={maxQuantity} onChange={this.setQuantity} />
                     <div className="button-addtocart">
                       <AddToCartButton product={product} variant={selectedVariant} addCartItem={this.addToCart} isAllOptionsSelected={isAllOptionsSelected} />
                     </div>
