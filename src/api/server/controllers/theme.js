110059a7079d2285b8a5100df0b16d9afcbfc9de
restmount <jsonmount@gmail.com>
2017-12-12 10:57:52 +0200

    Added Theme Placeholders

--- src/api/server/controllers/theme.js
+++ src/api/server/controllers/theme.js
@@ -4,6 +4,7 @@ const security = require('../lib/security');
 var ThemeService = require('../services/theme/theme');
 var ThemeSettingsService = require('../services/theme/settings');
 var ThemeAssetsService = require('../services/theme/assets');
+var ThemePlaceholdersService = require('../services/theme/placeholders');
 
 class ThemeController {
   constructor(router) {
@@ -21,6 +22,12 @@ class ThemeController {
 
     this.router.post('/v1/theme/assets', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.uploadFile.bind(this));
     this.router.delete('/v1/theme/assets/:file', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.deleteFile.bind(this));
+
+    this.router.get('/v1/theme/placeholders', security.checkUserScope.bind(this, security.scope.READ_THEME), this.getPlaceholders.bind(this));
+    this.router.post('/v1/theme/placeholders', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.addPlaceholder.bind(this));
+    this.router.get('/v1/theme/placeholders/:key', security.checkUserScope.bind(this, security.scope.READ_THEME), this.getSinglePlaceholder.bind(this));
+    this.router.put('/v1/theme/placeholders/:key', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.updatePlaceholder.bind(this));
+    this.router.delete('/v1/theme/placeholders/:key', security.checkUserScope.bind(this, security.scope.WRITE_THEME), this.deletePlaceholder.bind(this));
   }
 
   exportTheme(req, res, next) {
@@ -57,6 +64,46 @@ class ThemeController {
     ThemeAssetsService.deleteFile(req.params.file).then(() => {
       res.end()
     }).catch(next)
+  }
+
+  getPlaceholders(req, res, next) {
+    ThemePlaceholdersService.getPlaceholders().then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  getSinglePlaceholder(req, res, next) {
+    ThemePlaceholdersService.getSinglePlaceholder(req.params.key).then(data => {
+      if (data) {
+        res.send(data)
+      } else {
+        res.status(404).end()
+      }
+    }).catch(next);
+  }
+
+  addPlaceholder(req, res, next) {
+    ThemePlaceholdersService.addPlaceholder(req.body).then(data => {
+      res.send(data)
+    }).catch(next);
+  }
+
+  updatePlaceholder(req, res, next) {
+    ThemePlaceholdersService.updatePlaceholder(req.params.key, req.body).then(data => {
+      if (data) {
+        res.send(data)
+      } else {
+        res.status(404).end()
+      }
+    }).catch(next);
+  }
+
+  deletePlaceholder(req, res, next) {
+    ThemePlaceholdersService.deletePlaceholder(req.params.key).then(data => {
+      res.status(data
+        ? 200
+        : 404).end()
+    }).catch(next);
   }
 }
 
---
+++ src/api/server/services/theme/placeholders.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var mongo = require('../../lib/mongo');
+var parse = require('../../lib/parse');
+
+class ThemePlaceholdersService {
+  constructor() {}
+
+  getPlaceholders() {
+    return mongo.db.collection('themePlaceholders').find({}, { _id: 0 }).toArray();
+  }
+
+  getSinglePlaceholder(placeholderKey) {
+    return mongo.db.collection('themePlaceholders').findOne({key: placeholderKey}, { _id: 0 });
+  }
+
+  addPlaceholder(data) {
+    const field = this.getValidDocumentForInsert(data);
+    const placeholderKey = field.key;
+
+    return this.getSinglePlaceholder(placeholderKey)
+    .then(placeholder => {
+      if(placeholder){
+        // placeholder exists
+        return new Error('Placeholder exists');
+      } else {
+        // add
+        return mongo.db.collection('themePlaceholders').insertOne(field)
+        .then(res => this.getSinglePlaceholder(placeholderKey))
+      }
+    })
+  }
+
+  updatePlaceholder(placeholderKey, data) {
+    const field = this.getValidDocumentForUpdate(data);
+    return mongo.db.collection('themePlaceholders').updateOne({key: placeholderKey}, {
+      $set: field
+    }, {upsert: true}).then(res => this.getSinglePlaceholder(placeholderKey));
+  }
+
+  deletePlaceholder(placeholderKey) {
+    return mongo.db.collection('themePlaceholders').deleteOne({key: placeholderKey});
+  }
+
+  getValidDocumentForUpdate(data) {
+    if (Object.keys(data).length === 0) {
+      return new Error('Required fields are missing');
+    }
+
+    let field = {}
+
+    if (data.place !== undefined) {
+      field.place = parse.getString(data.place);
+    }
+
+    if (data.value !== undefined) {
+      field.value = parse.getString(data.value);
+    }
+
+    return field;
+  }
+
+  getValidDocumentForInsert(data) {
+    if (Object.keys(data).length === 0) {
+      return new Error('Required fields are missing');
+    }
+
+    let field = {}
+
+    if (data.key !== undefined) {
+      field.key = parse.getString(data.key);
+    }
+
+    if (data.place !== undefined) {
+      field.place = parse.getString(data.place);
+    }
+
+    if (data.value !== undefined) {
+      field.value = parse.getString(data.value);
+    }
+
+    return field;
+  }
+}
+
+module.exports = new ThemePlaceholdersService();
