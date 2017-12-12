0d2efc11711e2d9d37dc37015aa0beaa00334d47
restmount <jsonmount@gmail.com>
2017-12-12 14:52:34 +0200

    Remove unused texts

--- src/admin/client/routes/home.js
+++ src/admin/client/routes/home.js
@@ -3,12 +3,8 @@ import React from 'react'
 export default() => (
   <div>
     <div className="row">
-      <div className="col-xs-6">
-        Key performance indicators
-      </div>
-      <div className="col-xs-6">
-        Key performance indicators
-      </div>
+      <div className="col-xs-6"></div>
+      <div className="col-xs-6"></div>
     </div>
   </div>
 )
