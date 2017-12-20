f8a2f7ea8f92901f545db547827aba76a56db8c7
sonufrienko <sergey@onufrienko.com>
2017-12-20 14:48:05 +0200

    Added API to list files

--- src/api/server/controllers/files.js
+++ src/api/server/controllers/files.js
@@ -10,8 +10,17 @@ class FilesController {
   }
 
   registerRoutes() {
+    this.router.get('/v1/files', security.checkUserScope.bind(this, security.scope.READ_FILES), this.getFiles.bind(this));
     this.router.post('/v1/files', security.checkUserScope.bind(this, security.scope.WRITE_FILES), this.uploadFile.bind(this));
     this.router.delete('/v1/files/:file', security.checkUserScope.bind(this, security.scope.WRITE_FILES), this.deleteFile.bind(this));
+  }
+
+  getFiles(req, res, next) {
+    FilesService.getFiles()
+    .then((data) => {
+      res.send(data)
+    })
+    .catch(next);
   }
 
   uploadFile(req, res, next) {
