c6c6481e50051c96f8b560ca5040f6dadbb2a414
sonufrienko <sergey@onufrienko.com>
2017-12-21 17:10:44 +0200

    Show tags on Pages

--- src/admin/client/modules/pages/list/components/form.js
+++ src/admin/client/modules/pages/list/components/form.js
@@ -7,6 +7,8 @@ import FontIcon from 'material-ui/FontIcon';
 import {List, ListItem} from 'material-ui/List';
 
 const PageItem = ({page}) => {
+  const tags = page.tags && page.tags.length > 0 ? page.tags.join(', ') : '';
+
   return (
     <div>
       <Divider />
@@ -16,8 +18,12 @@ const PageItem = ({page}) => {
           leftIcon={page.is_system ? <FontIcon className="material-icons">lock_outline</FontIcon> : null}
           style={!page.enabled ? {color: 'rgba(0, 0, 0, 0.3)'} : {}}
           primaryText={<div className="row">
-            <div className="col-xs-6">{page.meta_title}</div>
-            <div className="col-xs-6" style={{ color: 'rgba(0, 0, 0, 0.4)' }}>{page.path}</div>
+            <div className="col-xs-8">
+              {page.meta_title}
+            </div>
+            <div className="col-xs-4" style={{ color: 'rgba(0, 0, 0, 0.4)' }}>
+              {tags}
+            </div>
           </div>}
         />
       </Link>
