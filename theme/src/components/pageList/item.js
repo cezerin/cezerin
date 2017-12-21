7f3775e445a55cf608c8a6a7a9978eba7b4c94cd
sonufrienko <sergey@onufrienko.com>
2017-12-21 16:51:29 +0200

    Added page list. Blog, news, docs, etc

---
+++ theme/src/components/pageList/item.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { themeSettings, text } from '../../lib/settings'
+
+const pad = number => number < 10 ? ('0' + number) : number;
+const formatDate = date => `${pad(date.getDate())}.${pad(date.getMonth() + 1)}.${date.getFullYear()}`;
+
+const PageListItem = ({page}) => (
+  <div className="page-item">
+    <h2><NavLink to={page.path}>{page.meta_title}</NavLink></h2>
+    <div className="date">
+      {formatDate(new Date(page.date_created))}
+    </div>
+    <div className="description">
+      {page.meta_description}
+    </div>
+  </div>
+)
+
+export default PageListItem
---
+++ theme/src/components/pageList/list.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { themeSettings, text } from '../../lib/settings'
+import PageListItem from './item'
+
+const PageList = ({pages}) => {
+  const items = pages ? pages.map((page, index) => (
+    <PageListItem key={index} page={page} />
+  )) : null;
+
+  return (
+    <div className="page-list">
+      {items}
+    </div>
+  )
+}
+
+export default PageList
