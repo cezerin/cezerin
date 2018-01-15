5db6294f380fafaad541a479e31833e46bd44cb4
sonufrienko <sergey@onufrienko.com>
2018-01-15 18:38:13 +0200

    Fixed. Tooltips show scrolls. Changed position.

--- src/admin/client/modules/orders/listHead/components/buttons.js
+++ src/admin/client/modules/orders/listHead/components/buttons.js
@@ -56,7 +56,7 @@ export default class Buttons extends React.Component {
         <Search value={search} setSearch={setSearch} />
         {selectedCount > 0 &&
           <span>
-            <IconButton touch={true} tooltip={messages.actions_delete} onClick={this.showDelete}>
+            <IconButton touch={true} tooltipPosition="bottom-left" tooltip={messages.actions_delete} onClick={this.showDelete}>
               <FontIcon color="#fff" className="material-icons">delete</FontIcon>
             </IconButton>
             <Dialog
