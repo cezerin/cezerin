e14e6ba57dc724de07a99c7e7558fece7bf2719e
sonufrienko <sergey@onufrienko.com>
2018-01-16 15:53:12 +0200

    Added search effect on focus and clear by Esc

--- theme/src/components/searchBox.js
+++ theme/src/components/searchBox.js
@@ -6,7 +6,8 @@ export default class SearchBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: props.value
+      value: props.value,
+      hasFocus: false
     };
   }
 
@@ -20,6 +21,12 @@ export default class SearchBox extends React.Component {
     }
   }
 
+  handleKeyDown = (e) => {
+    if (e.keyCode === 27) {
+      this.handleClear();
+    }
+  }
+
   handleSearch = () => {
     this.props.onSearch(this.state.value);
   }
@@ -29,11 +36,30 @@ export default class SearchBox extends React.Component {
     this.props.onSearch('');
   }
 
+  handleFocus = () => {
+    this.setState({hasFocus: true});
+  }
+
+  handleBlur = () => {
+    this.setState({hasFocus: false});
+  }
+
   render() {
+    const {hasFocus} = this.state;
     const placeholderText = themeSettings.search_placeholder && themeSettings.search_placeholder.length > 0 ? themeSettings.search_placeholder : text.searchPlaceholder;
-    
-    return <div className={'search-box ' + this.props.className}>
-      <input className="search-input" type="text" placeholder={placeholderText} value={this.state.value} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
+
+    return <div className={'search-box ' + this.props.className + (hasFocus ? ' has-focus' : '')}>
+      <input
+        className="search-input"
+        type="text"
+        placeholder={placeholderText}
+        value={this.state.value}
+        onChange={this.handleChange}
+        onKeyPress={this.handleKeyPress}
+        onKeyDown={this.handleKeyDown}
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
+      />
       <img className="search-icon-search" src="/assets/images/search.svg" alt={text.search} title={text.search} onClick={this.handleSearch}/>
       {this.state.value && this.state.value !== '' &&
         <img className="search-icon-clear" src="/assets/images/close.svg" onClick={this.handleClear} />
