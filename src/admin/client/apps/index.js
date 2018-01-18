0b9a0f7fd194be823d83f75b1e634c69368b9505
restmount <jsonmount@gmail.com>
2018-01-18 18:11:21 +0200

    Added Facebook Customer Chat app

--- src/admin/client/apps/index.js
+++ src/admin/client/apps/index.js
@@ -2,5 +2,6 @@ import * as GoogleAnalyticsApp from './google-analytics'
 import * as SiteVerificationApp from './site-verification'
 import * as JivositeApp from './jivosite'
 import * as FacebookSDKApp from './facebook-sdk'
+import * as FacebookCustomerChatApp from './facebook-customer-chat'
 
-export default [GoogleAnalyticsApp, SiteVerificationApp, JivositeApp, FacebookSDKApp];
+export default [GoogleAnalyticsApp, SiteVerificationApp, JivositeApp, FacebookSDKApp, FacebookCustomerChatApp];
