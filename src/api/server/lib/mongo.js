1972cb60ecc257c0878874c7843d6cf13013fbde
sonufrienko <sergey@onufrienko.com>
2018-01-25 17:32:51 +0200

    MongoDB auto reconnection

--- src/api/server/lib/mongo.js
+++ src/api/server/lib/mongo.js
@@ -1,18 +1,38 @@
 const settings = require('./settings');
 const winston = require('winston');
-const mongo = require('mongodb').MongoClient;
-const mongodbConnection = settings.mongodbServerUrl;
+const MongoClient = require('mongodb').MongoClient;
 
+const mongodbConnection = settings.mongodbServerUrl;
+const lastslashindex = mongodbConnection.lastIndexOf('/');
+const dbName = mongodbConnection.substring(lastslashindex  + 1);
+
+const RECONNECT_INTERVAL = 1000;
 const CONNECT_OPTIONS = {
   reconnectTries: 3600,
-  reconnectInterval: 1000
+  reconnectInterval: RECONNECT_INTERVAL
 }
 
-mongo.connect(mongodbConnection, CONNECT_OPTIONS, (err, client) => {
-  if(err){
-    winston.error('Failed connecting to MongoDB', err.message);
-  } else {
-    module.exports.db = client.db('shop');
-    winston.info('Successfully connected to MongoDB')
-  }
-});
+const onClose = () => {
+  winston.info('MongoDB connection was closed');
+}
+
+const onReconnect = () => {
+  winston.info('MongoDB reconnected');
+}
+
+const connectWithRetry = () => {
+  MongoClient.connect(mongodbConnection, CONNECT_OPTIONS, (err, client) => {
+    if(err){
+      winston.error('MongoDB connection was failed:', err.message);
+      setTimeout(connectWithRetry, RECONNECT_INTERVAL);
+    } else {
+      const db = client.db(dbName);
+      db.on('close', onClose);
+      db.on('reconnect', onReconnect);
+      module.exports.db = db;
+      winston.info('MongoDB connected successfully');
+    }
+  });
+};
+
+connectWithRetry();
