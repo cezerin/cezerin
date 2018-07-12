1328c82d0773673f4cd0f62f7abacdbc54e14aba
sonufrienko <sergey@onufrienko.com>
2018-07-12 21:56:45 +0300

    Upgrade logger

--- src/store/server/logger.js
+++ src/store/server/logger.js
@@ -2,11 +2,16 @@ const winston = require('winston');
 const LOGS_FILE = 'logs/server.log';
 
 winston.configure({
+  level: 'info',
+  format: winston.format.json(),
   transports: [
-    new (winston.transports.Console)({
-      colorize: true
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
     }),
-    new (winston.transports.File)({
+    new winston.transports.File({
       filename: LOGS_FILE,
       handleExceptions: true
     })
