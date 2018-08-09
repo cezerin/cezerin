6b96192a816b318a88f709b8cf9bf71484ca171a
Joaquín Gumucio <jjgumucio@gmail.com>
2018-08-09 01:26:29 -0400

    setup

--- src/api/server/setup-script.sh
+++ src/api/server/setup-script.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+echo "Running startup-script"
 apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 echo "Installing Docker CE"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,14 +9,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get update
-apt-get install docker-ce
+apt-get install -y docker-ce
 
 echo "Installing NGNX"
-apt update && apt install nginx-full
+apt-get update && apt -y install nginx-full
 
 echo "Installing NODE"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install -y nodejs
 
 echo "Update NPM"
 npm i -g npm
@@ -28,7 +28,7 @@ docker run --name store-db -d -p 27017:27017 -v /var/www/store-db:/data/db mongo
 
 echo "Installing Cezerin"
 cd /var/www
-git clone https://github.com/UTipsProjects/ucommerce.git
+git clone https://github.com/UTipsProjects/ucommerce.git cezerin
 
 echo "Installing dependencies"
 cd ucommerce && npm i
@@ -37,5 +37,5 @@ echo "Building ucommerce"
 npm run build
 
 echo "UCOMMERCE READY"
-echo "SSH in and run 'npm run setup <email-account> <domain-name> to finish setup!'"
+echo "SSH in and run 'npm run setup <email-account> <domain-name>' to finish setup!"
 echo "Run pm2 start process.json to start the application!"
